6ec92b24e01dea2fb374eceac4b6be871d00645e
tec-more <tec_more@yahoo.com>
2026-09-01 10:49:15 +0800

    修复el-button link 属性

--- web/src/layouts/MainLayout.vue
+++ web/src/layouts/MainLayout.vue
@@ -72,7 +72,7 @@
             <Expand v-else />
           </el-icon>
           <el-button 
-            type="text" 
+            link
             size="small" 
             @click="goBack" 
             class="back-btn"
